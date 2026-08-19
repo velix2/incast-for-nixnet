6f457ee69eec6d4741be22d03e34a7bdd406798b
Felix Daxenberger <felix.daxenberger@tum.de>
2026-08-19 12:43:11 +0000

    Modularize flake, drop flake-parts

---
+++ nixnet/experiment.nix
@@ -0,0 +1,98 @@
+{
+  pkgs,
+  lib,
+  nixnet,
+  incast,
+  
+  n ? 16,
+  blocks ? 200,
+  blockSizeBytes ? 1040000, # divided by n, it must be multiple of 1000  
+  rtoMinUs ? 200000,
+  bufferSizeKB ? 32,
+
+  measureRtt ? false,
+}:
+let 
+  mkAddress = i: "10.${toString (i / (254 * 254))}.${toString (i / 254)}.${toString ((lib.mod i 254) + 1)}";
+
+  mkServer = i:
+  let address = mkAddress i; in
+  {
+    nodes."server${toString i}" = {
+      packages = [ incast ];
+      networking.interfaces.${"eth${toString (i + 1)}"} = { 
+        ipv4.addresses = [
+          {
+            inherit address;
+            prefixLength = 8;
+          }
+        ];
+      };
+      scripts.main = {
+        exec =
+          ''
+            echo "Starting server on address ${address}"
+            server > /dev/null
+          '';
+        await = false;
+      };
+      workDir = null;
+    };
+    veths."eth${toString (i + 1)}" = {
+      a.node = "server${toString i}";
+      b.node = "br0";
+      netem.rateMbit = 1000;
+      netem.limit = 22; # roughly simulates 32 KB buffer
+    };
+  };
+
+  clientConfig =
+  {
+    nodes.client = {
+    packages = [ incast ] ++ lib.optional measureRtt pkgs.iputils;
+      networking.interfaces."eth0" = { 
+        ipv4.addresses = [
+          {
+            address = "10.0.0.1";
+            prefixLength = 8;
+          }
+        ];
+      };
+      scripts.main = {
+        exec =
+          let serverNamesFile = pkgs.writeText "server_names.txt" 
+          (builtins.concatStringsSep "\n" (map mkAddress (lib.range 1 n)));
+          stripeUnit = blockSizeBytes / n;
+          in
+          lib.optionalString measureRtt ''
+            ping -c 5 10.0.0.2 | grep "rtt" | tee ./rtt.txt
+          '' +
+          ''
+            sleep 1
+            # client [num of servers] [server names file] [port] [stripe unit] [server request unit] [num blocks]
+            client ${toString n} ${serverNamesFile} 65125 ${toString stripeUnit} 1 ${toString blocks} | tee ./stdout.txt   
+          '';
+        foreground = true;
+        await = true;
+      };
+    };
+    veths."eth0" = {
+      a.node = "client";
+      b.node = "br0";
+      netem.rateMbit = 1000;
+      netem.limit = bufferSizeKB * 1024 / 1500; # roughly simulates buffer size
+    };
+  };
+
+nodeList = [ clientConfig ] ++ map mkServer (lib.range 1 n);
+in
+{
+  arp = true;
+  arpPrefill = true;
+  sysctl = {
+      "net.ipv4.tcp_rto_min_us" = rtoMinUs;
+  };
+  bridges = [ "br0" ];
+  nodes = lib.mergeAttrsList (map (node: node.nodes) nodeList);
+  veths = lib.mergeAttrsList (map (node: node.veths) nodeList);
+}
--- nixnet/flake.nix
+++ nixnet/flake.nix
@@ -1,113 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixnet.url = "github:birneee/nixnet";
-    incast.url = "path:/home/felix/incast/app";
+    incast.url = "path:../app";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-
-      systems = inputs.nixnet.supportedSystems;
-      perSystem =
-        {
-          pkgs,
-          inputs',
-          lib,
-          ...
-        }:
-        let
-          nixnet = inputs'.nixnet.legacyPackages;
-          incast = inputs.incast.packages."x86_64-linux".default;
-          
-          n = 16;
-          blocks = 200;
-          blockSizeBytes = 1040000; # divided by n, it must be multiple of 1000  
-
-          mkAddress = i: "10.${toString (i / (254 * 254))}.${toString (i / 254)}.${toString ((lib.mod i 254) + 1)}";
-
-          mkServer = i: {
-            nodes."server${toString i}" = {
-              packages = [ incast ];
-              networking.interfaces.${"eth${toString (i + 1)}"} = { 
-                ipv4.addresses = [
-                  {
-                    address = mkAddress i;
-                    prefixLength = 8;
-                  }
-                ];
-              };
-              scripts.main = {
-                exec =
-                  ''
-                    echo "Starting server on address ${mkAddress i}"
-                    server > /dev/null
-                  '';
-                await = false;
-              };
-              workDir = null;
-            };
-            veths."eth${toString (i + 1)}" = {
-              a.node = "server${toString i}";
-              b.node = "br0";
-              netem.rateMbit = 1000;
-              netem.limit = 22; # roughly simulates 32 KB buffer
-            };
-          };
-          clientConfig =
-          {
-            nodes.client = {
-            packages = [ incast pkgs.iputils ];
-              networking.interfaces."eth0" = { 
-                ipv4.addresses = [
-                  {
-                    address = "10.0.0.1";
-                    prefixLength = 8;
-                  }
-                ];
-              };
-              scripts.main = {
-                exec =
-                  let serverNamesFile = pkgs.writeText "server_names.txt" 
-                  (builtins.concatStringsSep "\n" (map mkAddress (lib.range 1 n)));
-                  stripeUnit = blockSizeBytes / n;
-                  in
-                  ''
-                    ping -c 5 10.0.0.2 | grep "rtt" | tee ./rtt.txt
-                    sleep 1
-                    # client [num of servers] [server names file] [port] [stripe unit] [server request unit] [num blocks]
-                    client ${toString n} ${serverNamesFile} 65125 ${toString stripeUnit} 1 ${toString blocks} | tee ./stdout.txt   
-                  '';
-                foreground = true;
-                await = true;
-              };
-            };
-            veths."eth0" = {
-              a.node = "client";
-              b.node = "br0";
-              netem.rateMbit = 1000;
-              netem.limit = 22; # roughly simulates 32 KB buffer
-            };
-          };
-
-          nodeList = [ clientConfig ] ++ map mkServer (lib.range 1 n);
-          config = {
-            arp = true;
-            arpPrefill = true;
-            sysctl = {
-               "net.ipv4.tcp_rto_min_us" = 200000;
-            };
-            bridges = [ "br0" ];
-            nodes = lib.mergeAttrsList (map (node: node.nodes) nodeList);
-            veths = lib.mergeAttrsList (map (node: node.veths) nodeList);
-          };
-        in
-        {
-          packages.default = nixnet.mkExperiment config;
-          packages.mermaid = nixnet.mkMermaid config;
-          packages.mermaid-svg = nixnet.mkMermaidSvg config;
+    inputs:
+      let
+        system = "x86_64-linux";
+        pkgs = import inputs.nixpkgs { inherit system; };
+        nixnet = inputs.nixnet.legacyPackages.${system};
+        incast = inputs.incast.packages.${system}.default;
+        
+        config = import ./experiment.nix { 
+          inherit pkgs nixnet incast;
+          lib = pkgs.lib;
         };
-    };
+      in
+      {
+        packages.${system} = {
+          default = nixnet.mkExperiment config;
+          mermaid = nixnet.mkMermaid config;
+          mermaid-svg = nixnet.mkMermaidSvg config;
+        };
+      };
 }
